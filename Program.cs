3569177d53d84d87de9557c7c06113579d646525
curso.ads <curso.ads@LAB06-20>
2022-07-06 22:20:04 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+
+            for (a = 1; a <= 100; a++)
+            {
+                if (a % 2 == 0)
+                {
+                    Console.WriteLine(a);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
